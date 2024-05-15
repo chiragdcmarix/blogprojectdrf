6985f15d1159fc4948a50ad2504f5654a20e35b0
chiragdcmarix <77272627+chiragdcmarix@users.noreply.github.com>
2024-05-15 15:34:33 +0530

    Update start_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["Your_Instance_Public_IP"]/' /home/ubuntu/Your_project_folder_name/mysite/settings.py
+sed -i 's/\[]/\["3.89.249.80"]/' /root/blogprojectdrf/mysite/settings.py
 
 python manage.py migrate 
 # python manage.py makemigrations     
