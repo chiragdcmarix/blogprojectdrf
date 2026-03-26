bfb8714186c39fba408c1fb61a288804be5f361c
gitadmin <jinesh.patel@cmarix.com>
2026-03-26 18:20:16 +0530

    fix python

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -8,13 +8,13 @@ sudo apt-get install -y python3-venv python3-dev build-essential libyaml-dev
 
 rm -rf venv
 python3 -m venv venv
-
 source venv/bin/activate
 
 pip install --upgrade pip setuptools wheel
 
-# fix python 3.12 compatibility
-pip install "ruamel.yaml.clib==0.2.8"
-pip install "ruamel.yaml==0.18.6"
-
 pip install -r requirements.txt
+
+# force fix for python 3.12
+pip install --upgrade --force-reinstall \
+ruamel.yaml==0.18.6 \
+ruamel.yaml.clib==0.2.8
