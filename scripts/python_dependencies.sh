15d3dd8d6e57c444650f949db4e505fb8f6a8f80
gitadmin <jinesh.patel@cmarix.com>
2026-03-26 17:50:41 +0530

    assd

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -4,12 +4,17 @@ set -e
 cd /home/ubuntu/blogprojectdrf
 
 sudo apt-get update
-sudo apt-get install -y python3.10 python3.10-venv python3.10-dev build-essential
+sudo apt-get install -y build-essential python3-dev libyaml-dev
 
 rm -rf venv
-python3.10 -m venv venv
+python3 -m venv venv
 
 source venv/bin/activate
 
 pip install --upgrade pip setuptools wheel
+
+# fix for python 3.12
+pip install "ruamel.yaml.clib==0.2.8"
+pip install "ruamel.yaml==0.18.6"
+
 pip install -r requirements.txt
