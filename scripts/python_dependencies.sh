28730412e0a0d573851efa21d2984f5c727e4e1e
gitadmin <jinesh.patel@cmarix.com>
2026-03-26 17:37:20 +0530

    w

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -5,8 +5,22 @@ cd /home/ubuntu/blogprojectdrf
 
 sudo chown -R ubuntu:ubuntu .
 
+sudo apt-get update
+
+sudo apt-get install -y \
+python3-dev \
+build-essential \
+libjpeg-dev \
+zlib1g-dev \
+libfreetype6-dev \
+liblcms2-dev \
+libwebp-dev \
+libtiff-dev \
+tk-dev \
+gcc
+
 python3 -m virtualenv venv
 source venv/bin/activate
 
-pip install --upgrade pip
+pip install --upgrade pip wheel setuptools
 pip install -r requirements.txt
